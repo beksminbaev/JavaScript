b429bc3ab83412d3989396f6c9b525e2a963d676
BekzatMinbaev <bekatut00@email.com>
2021-12-10 17:17:45 -0800

    starter files added

---
+++ 11-29/mycode.js
@@ -0,0 +1,74 @@
+
+// console.log("Secret message");
+// document.write("heyy");
+// // alert('watch out!');
+
+// let isFloridaWarm = true;
+// console.log (isFloridaWarm);
+// console.log(!isFloridaWarm);
+// console.log(11<5);
+// console.log(11>5);
+// let myCar = 2;
+// let yourCars = 5;
+// numberOfCars = myCar+yourCars;
+// console.log("number os cars:" + numberOfCars)
+
+// let firstName = "Barak"
+// let lastName = "Obama"
+// let myName = firstName + " " + lastName 
+// console.log("Full name is " + myName)
+// let numberOfFruits = "6"
+// let typeOfFruit = "banana"
+// let allTheFruits = " I Have " + numberOfFruits + " " + typeOfFruit + "!"
+// console.log(allTheFruits)
+
+
+// let preTip = "20"
+// let tip = "0.1233"
+// let sum = preTip + (preTip * tip) 
+// console.log("Your total bill with tip will be: " + sum)
+
+
+// function outputTheFact () {
+    
+//     console.log("The Population of the USA is #350M")
+// }
+// outputTheFact();
+
+// let fact2 = function (){
+//     console.log("The founder of America is Christopher Columb")
+// }
+// fact2();
+// function addNumbers(a,b,c){
+//     console.log (a*b+c)
+// }
+// addNumbers(5,5,30);
+//   function fullName (firstName,lastName){
+//       console.log("Mikey" , "Baji")
+//   }
+// function greeting (name){
+//     return "Hello," +name;
+// }
+// greeting("Bekzat")
+
+
+// let ex1 = 1
+// let ex2 = 2
+// if(ex2>=2){
+// console.log ("yes , you are right " + ex1) 
+// }
+// else{
+//     console.log("you are very wrong")
+// }
+//  let temperature = 60
+//   if(temperature < 60){
+//       console.log ("you better drink cold water")
+//   } else {
+//       console.log ("dont drink cold water, but warm")
+//   }
+//   let age = 35
+//   if (age >= 23){
+//   console.log ("you cannot enter night clubs")}
+//   else if (age <= 20){
+//       console.log ("you can enter clubs")
+//   }
